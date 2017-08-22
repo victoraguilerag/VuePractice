11274aaf6b3efdbf90b81b3764d89a0d6281b69f
root <victoraguilerag17@gmail.com>
2017-08-21 23:27:00 -0400

    Fixes URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://victoraguilerag.github.io/VuePractice/dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for='country in countries' v-bind:value="country.value") {{ country.name}}
