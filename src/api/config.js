6aaf345331bfa91e6595254c9cde059e7ae92a96
root <victoraguilerag17@gmail.com>
2017-08-21 23:17:35 -0400

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+  appname:'platzimusic',
+  apiKey: 'd6762294dbc33332597816f2f1c3453a',
+  secret: 'e49abece44a8f0b4ac1f6cdf41f55d9b',
+  registeredTo: 'victoraguilerag'
+}
+
+
+// Application name	platzimus
+// API key	d6762294dbc33332597816f2f1c3453a
+// Shared secret	e49abece44a8f0b4ac1f6cdf41f55d9b
+// Registered to	victoraguilerag
---
+++ src/components/Artist.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  li.artist
+    h2: a(:href='artist.url' target='_blank') {{artist.name}}
+    img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: [ 'artist' ]
+}
+</script>
+
+<style lang='stylus' scoped>
+li.artist
+
+    margin 10px 61px
+    width 10%
+    max-height 300px
+    padding 20px 10px
+    border lightcoral thin solid
+    box-shadow inset 2px 2px 2px rgba(104,104,104,0.5)
+    border-radius 5px
+    background-color white
+
+</style>
